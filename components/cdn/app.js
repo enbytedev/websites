c6bb457b7d2be5dcd518e82b7a5385743c6caab8
enbytedev <me@enbyte.dev>
2022-08-13 18:53:41 -0700

    Fixed issues with CDN component

--- components/cdn/app.js
+++ components/cdn/app.js
@@ -9,23 +9,23 @@ app.use(express.static('public'));
 const cdnPath = "./components/cdn/assets"
 app.get("/banner", (req, res) => {
 	var banner = new Array(`${cdnPath}/banner/bash1.png`, 
-        `${bannerPath}/bash2.png`,
-		`${bannerPath}/html1.png`,
-		`${bannerPath}/python1.png`,
-		`${bannerPath}/java1.png`,
-		`${bannerPath}/java2.png`,
-		`${bannerPath}/java3.png`,
-		`${bannerPath}/java4.png`,
-		`${bannerPath}/js1.png`,
-		`${bannerPath}/js2.png`,
-		`${bannerPath}/js3.png`,
-		`${bannerPath}/js4.png`);
+        `${cdnPath}/banner/bash2.png`,
+		`${cdnPath}/banner/html1.png`,
+		`${cdnPath}/banner/python1.png`,
+		`${cdnPath}/banner/java1.png`,
+		`${cdnPath}/banner/java2.png`,
+		`${cdnPath}/banner/java3.png`,
+		`${cdnPath}/banner/java4.png`,
+		`${cdnPath}/banner/js1.png`,
+		`${cdnPath}/banner/js2.png`,
+		`${cdnPath}/banner/js3.png`,
+		`${cdnPath}/banner/js4.png`);
 	var rNum = Math.floor(Math.random() * banner.length);
 	res.set('cache-control', 'no-cache,max-age=0,no-store,s-maxage=0,proxy-revalidate')
 	res.sendFile(banner[rNum], { root: "."});
 });
 
-app.get("/install", (req, res) => {
+app.get("/", (req, res) => {
 	res.sendFile(`${cdnPath}/scripts/install.sh`, { root: "."});
 });
 
