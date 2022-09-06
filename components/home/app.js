f9082f6ee2e35086a75d8d4f5eb1f40f32056e1e
enbytedev <me@enbyte.dev>
2022-09-05 22:06:58 -0700

    Updated site for now!

--- components/home/app.js
+++ components/home/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static('components/home/public'));
+app.use('/public', express.static(path.resolve('./components/home/public/')));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
